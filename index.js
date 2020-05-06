dd39a85d3c5ccd4fd987e6021beec698440fa724
ent3r <32072697+ent3r@users.noreply.github.com>
2020-05-06 14:20:14 +0200

    Add comments

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const { Client, MessageEmbed } = require("discord.js");
-const client = new Client();
-require("dotenv").config();
+const { Client } = require("discord.js"); // We need the client object in order to make the discord client
+const client = new Client(); // Making the client
+require("dotenv").config(); // Standard dotenv stuff
 
+/*
+When the client is ready, console.log it's tag
+*/
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+/*
+Every time there is a message, run the rfc.js file
+*/
 client.on("message", (message) => {
-  require("./rfc.js").run(client, message)
+  require("./rfc.js").run(client, message);
 });
 
+
+/*
+Log the bot in when everything is ready
+*/
 client.login(process.env.TOKEN); //Add your token to a .env file
--- rfc.js
+++ rfc.js
@@ -1,20 +1,32 @@
-const fetch = require("axios");
-const { MessageEmbed } = require("discord.js");
+const fetch = require("axios"); // Using axios because of it's support for JSON parsing
+const { MessageEmbed } = require("discord.js"); // We need MessageEmbed to make discord embeds
 
-const rexp = /rfc ?\d{1,5}/i;
-const url = "https://www.ietf.org/rfc/";
+const regexp = /rfc ?\d{1,5}/i; // The regex that matches the rfc thing
+const url = "https://www.ietf.org/rfc/"; // Where to download rfcs from
 
+/**
+ *Standard module stuff, to be able to run the command
+*/
 module.exports = {
-  name: "RFC",
-  enabled: true,
+  /**
+   *Runs a check on a message to get an rfc, and if one is found, return info on it
+   *
+   * @param {*} client The client object
+   * @param {*} message The message object
+   * @param {*} [args=null] Parsed message arguments
+   * @returns null
+   */
   run: function (client, message, args = null) {
     if (message.author.bot) return; // We won't respond to bots
-    let rematch = message.content.match(rexp); // Use regex to check wether there is an rfc in the message
+    let rematch = message.content.match(regexp); // Use regex to check wether there is an rfc in the message
     if (rematch) {
       // And if there is
       console.log(rematch); // Log it to the console
       let rfcNumber = parseInt(rematch[0].replace(" ", "").substring(3), 10); // Get the rfc's number
-      fetch({ url: `${url}rfc${rfcNumber}.json`, method: "GET" }) // And download it from the website
+      fetch({
+        url: `${url}rfc${rfcNumber}.json`,
+        method: "GET",
+      }) // And download it from the website
         .then((response) => {
           // Then we are using that json object
           let msg = new MessageEmbed() // To make a new embed
@@ -22,7 +34,7 @@ module.exports = {
             .setDescription(
               response.data["abstract"] // If it has a description
                 ? response.data["abstract"] // Use it
-                : "No description for this RFC" // Else say there is no description
+                : "No description was found for this RFC" // Else say there is no description
             )
             .setAuthor(`RFC${rfcNumber}`) // SetAuthor is used for displaying which rfc it is
             .setColor("#00ff00"); // Sets the color to green
